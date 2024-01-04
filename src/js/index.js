2191b98451205b3fc3d339cf80128b36920e31d7
Emmanuel <154647800+egarboggini@users.noreply.github.com>
2024-01-03 22:54:00 -0300

    uping js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,24 @@
-//passar o mouse por cima dos personagens e fazer eles mudarem os personagens grandes
+const personagens = document.querySelectorAll(".personagem");
 
+personagens.forEach((personagem) => {
+    personagem.addEventListener("mouseenter", () => {
+
+        const idSelecionado = personagem.attributes.id.value;
+
+        if (idSelecionado === 'ultron') return;
+
+        const personagemSelecionado = document.querySelector(".selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+
+        personagem.classList.add("selecionado");
+
+        const imagemJogador1 = document.getElementById('personagem-jogador-1');
+        imagemJogador1.src = `./src/images/${idSelecionado}.png`;
+
+        const nomeJogador1 = document.getElementById('nome-jogador-1');
+        const nomeSelecionado = personagem.getAttribute('data-name');
+
+        nomeJogador1.innerHTML = nomeSelecionado;
+    });
+
+})
